c2916b8d2467a7e11e51434a3532868c9e9526e0
maxifranco99 <100171895+maxifranco99@users.noreply.github.com>
2022-04-18 18:46:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-//alert()
-for(let i=0; i<=10; i++){
-    console.log(`interacion ${i}`);
-}
-console.log('fin del ciclo');
-
-var dias = ["lunes", "martes", "miercoles", "jueves", "viewrnes", "sabado", "domingo"];
-
-var numeroDias = dias.length -1;
-var i = 0
-while (i <= numeroDias) {
-    document.write( dias [i] + "<br>" );
-    i++;
+function convertir(){
+    var valore = parseInt(document.getElementById("valor").value);
+    var resultado = 0;
+    var dolar = 192;
+    var euro = 210;
+    if (document.getElementById("uno").checked){
+        resultado = valore / dolar;
+        alert("El cambio de pesos a Dolares es $" + resultado.toFixed(2));
+    }
+    else if (document.getElementById("dos").checked){
+        resultado = valore / euro;
+        alert("El cambio de pesos a Euros es $" + resultado.toFixed(2));
+    }
+    else{
+        alert("Tienes que completar todos los requisitos")
+    }
 }
